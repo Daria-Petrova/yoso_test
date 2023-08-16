5b2413adcaa07bd4470952dc2182e585cbdf705a
Daria-Petrova <o.mine.God@gmail.com>
2023-08-16 18:27:40 +0300

    Поменян способ сохранения истории в url

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import { createApp } from 'vue'
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import App from './App.vue'
 import UserPage from './components/user/UserPage.vue'
 import UserList from './components/user/UserList.vue'
@@ -7,29 +7,29 @@ import ApplicationForm from './components/application/ApplicationForm.vue'
 import ApplicationList from './components//application//ApplicationList.vue'
 
 const router = createRouter({
-    routes: [{
-      path: '/',
-      name: 'user-list',
-      component: UserList
-      },
-      {
-        path: '/request', 
-        name: 'application-form',
-        component: ApplicationForm
-      },
-      {
-        path: '/request-list',
-        name: 'application-list',
-        props: true,
-        component: ApplicationList
-      },
-      {
-        path: '/user/:id',
-        name: 'user-page',
-        component: UserPage
-      }
-    ],
-    history: createWebHistory()
+  history: createWebHashHistory(),
+  routes: [{
+    path: '/',
+    name: 'user-list',
+    component: UserList
+    },
+    {
+      path: '/request', 
+      name: 'application-form',
+      component: ApplicationForm
+    },
+    {
+      path: '/request-list',
+      name: 'application-list',
+      props: true,
+      component: ApplicationList
+    },
+    {
+      path: '/user/:id',
+      name: 'user-page',
+      component: UserPage
+    }
+  ]
   })
 
 createApp(App).use(router).mount('#app')
